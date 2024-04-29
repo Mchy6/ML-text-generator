eaff302464bf6207d1c82d911fe7f9ac3d9e6ad6
Matthew Bailey <mmchy6@gmail.com>
2024-04-29 17:05:20 -0600

    initialized files

---
+++ similar_text_generator.py
@@ -0,0 +1,8 @@
+def generate_similar_text(input_text: str = None) -> str:
+    if input_text is None:
+        with open("input.txt", "r") as file:
+            input_text = file.read()
+
+
+
+    return "Not implemented yet"
